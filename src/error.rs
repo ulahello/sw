cc6169694af05178ec7c62dbc56d346b36c64b14
Ula Shipman <ula.hello@mailbox.org>
2022-04-02 12:55:30 -0700

    chore(error): allow module name repetitions

--- src/error.rs
+++ src/error.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#![allow(clippy::module_name_repetitions)]
+
 use std::fmt;
 use std::io;
 use std::num::ParseFloatError;
